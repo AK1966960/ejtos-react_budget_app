75f4f93ab2366e6ec646e07988fb0d548a3769bf
AK1966960 <154001847+AK1966960@users.noreply.github.com>
2024-02-02 10:18:44 +0100

    Update AllocationForm.js

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -59,12 +59,13 @@ const AllocationForm = (props) => {
                 <option value="Reduce" name="Reduce">Reduce</option>
                   </select>
 
+                  <span style={{paddingLeft:'30px'}}>£</span>
                     <input
                         required='required'
                         type='number'
                         id='cost'
                         value={cost}
-                        style={{ marginLeft: '2rem' , size: 10}}
+                        style={{ marginLeft: '10px' , size: 10}}
                         onChange={(event) => setCost(event.target.value)}>
                         </input>
 
